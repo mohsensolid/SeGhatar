1d719885912914eb5b7a4d375168b835a30d0e1f
mohsen <mohsen.ashouri.ms@gmail.com>
2018-10-16 07:42:19 +0330

    Initial Commit

---
+++ SeGhatar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeGhatar
+//
+//  Created by MAC on 7/24/1397 AP.
+//  Copyright © 1397 MohsenSolid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
